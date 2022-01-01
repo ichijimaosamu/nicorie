3961c7e8473016a9eddb3211cfd21227c83d745a
ichijimaosamu <ichijimaosamu@gmail.com>
2022-01-01 12:27:22 +0900

    アニメーション調整あど（現状スマホではアニメーションが働かないように）

--- js/top.js
+++ js/top.js
@@ -1,20 +1,25 @@
-var webStorage = function(){
-	if(sessionStorage.getItem('access')){
-		$('.start').remove();
-	} else {
-		setTimeout(function(){
-			$('.start p').fadeIn(2000);
-			$('.start p').delay(1000).queue(function() {
-				$(this).addClass('animate').dequeue();
-			});
-		},1000);
+var $win = $(window);
+var windowWidth = $win.width();
 
-		setTimeout(function(){
-			$('.start').fadeOut(500);
-		},6500);
+if (windowWidth > 768) {
+	var webStorage = function(){
+		if(sessionStorage.getItem('access')){
+			$('.start').remove();
+		} else {
+			setTimeout(function(){
+				$('.start p').fadeIn(2000);
+				$('.start p').delay(1000).queue(function() {
+					$(this).addClass('animate').dequeue();
+				});
+			},1000);
 
-		sessionStorage.setItem('access', 0);
+			setTimeout(function(){
+				$('.start').fadeOut(500);
+			},6500);
+
+			sessionStorage.setItem('access', 0);
+		}
 	}
-}
 
-webStorage();
+	webStorage();
+}
