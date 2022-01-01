27bb16f3628e60af541468aa483bcc42993c8d31
ichijimaosamu <ichijimaosamu@gmail.com>
2022-01-01 10:10:25 +0900

    プロフィールページ追加

--- common/header.php
+++ common/header.php
@@ -6,8 +6,8 @@
 <nav class="header__nav nav">
 <ul class="header__nav__list">
 <li class="header__nav__item"><a href="<?php echo $base_url . '/'; ?>"><span>Home</span></a></li>
+<li class="header__nav__item"><a href="<?php echo $base_url . '/'; ?>profile/"><span>Profile</span></a></li>
 <li class="header__nav__item"><a href="<?php echo $base_url . '/'; ?>work/" class="-noready"><span>Work</span></a></li>
-<li class="header__nav__item"><a href="<?php echo $base_url . '/'; ?>profile/" class="-noready"><span>Profile</span></a></li>
 <li class="header__nav__item"><a href="<?php echo $base_url . '/'; ?>product/" class="-noready"><span>Product</span></a></li>
 <li class="header__nav__item"><a href="<?php echo $base_url . '/'; ?>contact/"><span>Contact</span></a></li>
 </ul>
---
+++ profile/index.php
@@ -0,0 +1,155 @@
+<?php
+if ($_SERVER["HTTP_HOST"] == 'localhost') {
+	include_once '../common/base_url.php';
+} else {
+	include_once ($_SERVER['DOCUMENT_ROOT'] .'/common/base_url.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title></title>
+<meta name="keywords" content="印刷,チラシ,パンフレット,会社案内,ホームページ制作,動画制作,のぼり,デザイン事務所,デザインスタジオ,撮影,神戸市">
+<meta name="description" content="神戸市のデザインスタジオ、ニコリエデザインプランニング。地域密着型のデザイン事務所を目指しています。">
+<?php
+if ($_SERVER["HTTP_HOST"] == 'localhost') {
+	include_once '../common/common_head.php';
+} else {
+	include_once ($_SERVER['DOCUMENT_ROOT'] .'/common/common_head.php');
+}
+?>
+<link rel="stylesheet" href="<?php echo $base_url . '/'; ?>css/profile.css" />
+</head>
+<body id="profile" itemscope="itemscope" itemtype="http://schema.org/WebPage">
+
+<div id="container">
+<!--container-->
+
+<?php
+if ($_SERVER["HTTP_HOST"] == 'localhost') {
+	include_once '../common/header.php';
+} else {
+	include_once ($_SERVER['DOCUMENT_ROOT'] .'/common/header.php');
+}
+?>
+
+<div id="content">
+<!--content-->
+
+<main role="main">
+
+<article class="main">
+<!--main-->
+
+<h1 class="main__logo"><img src="<?php echo $base_url . '/'; ?>img/profile.svg" alt="Profile"></h1>
+
+<div class="main__box">
+<p class="main__box__lowerlogo"><img src="<?php echo $base_url . '/'; ?>img/logo_green.svg" alt="nicorie"></p>
+<p class="main__box__catch">笑顔を描く<br>アトリエ</p>
+
+<h2 class="main__box__title"><img src="<?php echo $base_url . '/'; ?>img/main_box_title.svg" alt="ニコリエデザインプランニング"></h2>
+
+<p class="main__box__lead"><span class="main__box__lead__middle">お客様の</span><br>
+<span class="main__box__lead__big">届けたい思い<br>
+伝えたい価値<br>
+広げたい魅力</span>
+それらをカタチにしてご提供することが<br>
+私たちの仕事です
+<span class="main__box__lead__footer">情報加工の力で<br>
+つながる全ての人々を<br>
+笑顔にします</span></p>
+
+<div class="concept">
+<h3 class="concept__title">Design Concept</h3>
+
+<dl>
+<dt class="concept__term">“身の丈にあったデザイン”</dt>
+<dd class="concept__description">プロ同士で褒め合うような高尚なデザインではなく、<br>
+もっと誰にでも分かりやすく、伝わりやすいビジュアルで、<br>
+身近に感じられながらもワンランク上のデザインを<br>
+ご提供していきたいと思っています。</dd>
+</dl>
+</div>
+
+<div class="representative">
+<h3 class="representative__title">代表　山本達也</h3>
+
+<p class="representative__text">20年間、カメラマン兼グラフィックデザイナーとして、神戸の老舗印刷出版社勤務。<br>
+カメラマンとしては、学校案内・広報関連などを中心に、神戸市を代表する広報誌の表紙を3年間、<br>
+消防局の広報撮影5年間、神戸市議会議員の広報撮影なども務める。<br>
+某区写真コンク—ルでは特別審査員を務め、写真教室の講師も担当。<br>
+グラフィックデザインでは、常に新規獲得を中心に任され、チーフデザイナーとして務める。<br>
+2011年、大阪のものづくり企業に転職し、デザイン雑貨ブランドを代表と二人でゼロから立ち上げる。<br>
+6年間、商品企画開発をはじめ、商品プロデュース、広告販促物制作、製造・生産・販売管理など<br>
+あらゆる業務を経てブランドの構築スキルを習得する。<br>
+ドイツ「ambiente」、東京「interiorlifestyle」など、国内外の大規模展示会への出展約10回。<br>
+また、TV番組「探偵！ナイトスクープ」から出演オファーを受けるなど、テレビ出演も経験。<br>
+2017年、古巣の印刷出版社より復職を打診され、アートディレクターとして再就職。<br>
+印刷からの脱却を掲げ、様々な特殊案件に挑み続ける。<br>
+2021年、さらなる自分の可能性を追求するため独立。<br>
+「ニコリエデザインプランニング」を立ち上げる。</p>
+
+<h4 class="representative__subtitle">■雑貨ブランドで開発した商品の受賞</h4>
+<p class="representative__text">ドイツで開かれる世界最大見本市「ambiente 2014/2015/2016」にて、<br>
+3年間連続新作優秀プロダクト「Trends」に選ばれる。<br>
+2017年、イギリスの「Wallpaper Design Awards 2018」を受賞。</p>
+
+<h4 class="representative__subtitle">■開発を担当した商品のメディア露出一例</h4>
+<p class="representative__text">NHK国際放送「NHK WORLD」にて特集を組まれる。<br>
+NHK「おはよう日本」にて話題の商品として紹介。<br>
+雑貨雑誌「MONOQLO」にて1ページで掲載。<br>
+雑貨雑誌「MONOマガジン」にて1/2ページで掲載。<br>
+ファッション雑誌「MADURO」にて2ページ見開きで掲載。<br>
+パソコン雑誌「MAC FAN」にてクローズアップアイテムとして掲載。<br>
+全商品、発表ごとに「日経MJ」話題の新製品のコーナーに掲載される。<br>
+その他、ピアノ雑誌、婦人雑誌、建築雑誌、新聞やwebニュースなど掲載多数。</p>
+
+<h4 class="representative__subtitle">■プライベート</h4>
+<p class="representative__text">社会人バドミントンチーム「<a href="https://www.coccoclub.org/" target="_blank" rel="noopener">COCCO CLUB</a>」代表<br>
+国際交流団体「<a href="https://www.oiw1989.com/" target="_blank" rel="noopener">大阪インザワールド</a>」所属</p>
+</div>
+
+<p class="works__logo"><img src="<?php echo $base_url . '/'; ?>img/logo_green.svg" alt="ニコリエデザインプランニング"></p>
+
+</div>
+
+<p class="main__address">&#12306;654-0162 兵庫県神戸市須磨区神の谷6丁目2-106-204<br>
+info@nicorie.com<br>
+070-1552-7400<br>
+神戸市須磨区・長田区・垂水区・西区・明石市</p>
+
+<!--/main-->
+</article>
+
+</main>
+
+<!--/content-->
+</div>
+
+<!--/container-->
+</div>
+
+<?php
+if ($_SERVER["HTTP_HOST"] == 'localhost') {
+	include_once '../common/common_script.php';
+} else {
+	include_once ($_SERVER['DOCUMENT_ROOT'] .'/common/common_script.php');
+}
+?>
+
+<?php if(false) { ?>
+<!--sp_menu-->
+<?php
+if ($_SERVER["HTTP_HOST"] == 'localhost') {
+	include_once '../common/sp_menu.php';
+} else {
+	include_once ($_SERVER['DOCUMENT_ROOT'] .'/common/sp_menu.php');
+}
+?>
+<!--/sp_menu-->
+<?php } ?>
+
+<script src="<?php echo $base_url . '/'; ?>js/top.js"></script>
+</body>
+</html>
